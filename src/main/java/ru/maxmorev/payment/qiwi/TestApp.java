8f66cee14639caa2a91e3c6632fb38a1a632dfe3
maxmorev <maxmorev@gmail.com>
2019-02-25 16:02:29 +0300

    Simple test app

--- src/main/java/ru/maxmorev/payment/qiwi/TestApp.java
+++ src/main/java/ru/maxmorev/payment/qiwi/TestApp.java
@@ -1,4 +1,32 @@
 package ru.maxmorev.payment.qiwi;
 
+import ru.maxmorev.payment.qiwi.response.Payment;
+
+import java.util.List;
+
 public class TestApp {
+
+
+    public static void main(String args[]) {
+
+        String token = "";
+        String phone = "79263926369";
+
+        QIWI qiwi = new QIWI( phone, token);
+
+        System.out.println("2. TEST QIWI API GET BALANCE RU");
+
+        System.out.println(qiwi.getBalanceRU());
+
+
+        List<Payment> payments = qiwi.getPaymentsLast(3);
+
+        for (Payment pay : payments) {
+            System.out.println(pay.toString());
+        }
+
+
+    }
+
+
 }

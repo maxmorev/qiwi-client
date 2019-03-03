350a813b677b23bcf07c5c8783d8f142a2f0623f
maxmorev <maxmorev@gmail.com>
2019-03-04 02:14:19 +0300

    Refactoring

--- src/main/java/ru/maxmorev/payment/qiwi/response/Wallet.java
+++ src/main/java/ru/maxmorev/payment/qiwi/response/Wallet.java
@@ -1,13 +1,16 @@
 package ru.maxmorev.payment.qiwi.response;
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
-public class Wallet {
-	
+public class Wallet extends QiwiResponse {
+
+	public static final String ALIAS_RU_ACCOUNT = "qw_wallet_rub";
+
 	private List<Account> accounts;
 	
 	public List<Account> getAccounts() {
@@ -21,17 +24,14 @@ public class Wallet {
 	@JsonIgnore
 	private double ruBalance;
 	
-	public double getRuBalance() {
-		if(this.accounts==null) {
-			//this.hasBalance = false;
-			return -404.0;
+	public double getBalanceRub() {
+		if(accounts==null){
+			return 0.0d;
 		}
-		for (Account account : accounts) {
-		    //System.out.println(element);
-			if(account.getAlias().equals("qw_wallet_rub")) {
-				this.ruBalance = account.getBalance().getAmount();
-				return ruBalance;
-			}
+		Optional<Account> account = accounts.stream().filter(a->a.getAlias().equals(ALIAS_RU_ACCOUNT)).findFirst();
+
+		if(account.isPresent()){
+				this.ruBalance = account.get().getBalance().getAmount();
 		}
 		return this.ruBalance;
 	}
